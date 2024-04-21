647fa97135c81b637724aef3b0c041c1000b03d4
Sadaqat-Abbasi <sadaqataliabbasi110@gmail.com>
2024-04-21 16:44:27 +0500

    simple atm project

---
+++ index.js
@@ -0,0 +1,34 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let mybalance = 10000;
+let mypin = 1234;
+let answerpin = await inquirer.prompt([
+    { name: "pincode", message: "enter your four digit pin...", type: "number" },
+]);
+if (answerpin.pincode === mypin) {
+    console.log("correct pin code !");
+    let answeroperator = await inquirer.prompt([
+        {
+            name: "operation",
+            message: "select one option....",
+            type: "list",
+            choices: ["withdraw", "check balance"],
+        },
+    ]);
+    if (answeroperator.operation === "withdraw") {
+        let answeramount = await inquirer.prompt([
+            {
+                name: "amount",
+                message: "enter your amount....",
+                type: "number",
+            },
+        ]);
+        console.log(`your remaining account balance is `, (mybalance -= answeramount.amount));
+    }
+    else if (answeroperator.operation === "check balance") {
+        console.log(`your current account balance is `, mybalance);
+    }
+}
+else {
+    console.log("invalid pin pin code");
+}
